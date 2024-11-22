73e0a65d56f11e640d2440f183b85f37d6a6e892
David Longnecker <dlongnecke-cray@users.noreply.github.com>
2024-11-21 20:04:59 -0800

    Add code to dynamically load a C library and intern symbols
    Signed-off-by: David Longnecker <dlongnecke-cray@users.noreply.github.com>

--- compiler/resolution/preFold.cpp
+++ compiler/resolution/preFold.cpp
@@ -1710,10 +1710,12 @@ static Expr* preFoldPrimOp(CallExpr* call) {
   case PRIM_IS_FCF_TYPE: {
     Type* t = call->get(1)->typeInfo();
 
-    if (t->symbol->hasFlag(FLAG_FUNCTION_CLASS))
+    if (t->symbol->hasFlag(FLAG_FUNCTION_CLASS) ||
+        isFunctionType(t)) {
       retval = new SymExpr(gTrue);
-    else
+    } else {
       retval = new SymExpr(gFalse);
+    }
 
     call->replace(retval);
 
