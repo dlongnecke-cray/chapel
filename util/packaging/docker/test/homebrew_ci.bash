cf29455e800261c09ae20ff0b368406fb88a9e79
Anna Rift <anna.rift@hpe.com>
2026-05-01 11:27:27 -0700

    Only remove homebrew_ci test container on next run
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/packaging/docker/test/homebrew_ci.bash
+++ util/packaging/docker/test/homebrew_ci.bash
@@ -12,7 +12,9 @@ docker build . --load --platform linux/amd64 -t homebrew_ci
 docker image inspect homebrew_ci
 
 # Start the container and run a script to check homebrew install inside the running container
-docker run --platform linux/amd64 --rm -i homebrew_ci /bin/bash < ${CHPL_HOME}/util/packaging/docker/test/brew_install.bash
+CONTAINER_NAME=homebrew-ci-test
+docker container rm --force --volumes "$CONTAINER_NAME"
+docker run --platform linux/amd64 -i --name "$CONTAINER_NAME" homebrew_ci /bin/bash < ${CHPL_HOME}/util/packaging/docker/test/brew_install.bash
 CONTAINER_RUN=$?
 if [ $CONTAINER_RUN -ne 0 ]
 then
